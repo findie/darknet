2acf95c02dea6284de9e8286de8cb01b8ea76346
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2020-05-25 15:14:29 +0300

    add: rgb_to_image helper function
    useful for FFI interfaces where GC might free
    original image data at any time

--- src/image.c
+++ src/image.c
@@ -767,6 +767,26 @@ image make_random_image(int w, int h, int c)
     return out;
 }
 
+image rgb_to_image(int w, int h, int c, unsigned char* data)
+{
+  image out = make_empty_image(w,h,c);
+  out.data = malloc(w*h*c*sizeof(float));
+
+  int step = w*c;
+  int i, j, k;
+  float mult = 1/255.;
+
+  for(i = 0; i < h; ++i){
+    for(k= 0; k < c; ++k){
+      for(j = 0; j < w; ++j){
+        out.data[k*w*h + i*w + j] = data[i*step + j*c + k] * mult;
+      }
+    }
+  }
+
+  return out;
+}
+
 image float_to_image(int w, int h, int c, float *data)
 {
     image out = make_empty_image(w,h,c);
